7fa44dceebcaf7d716fac87cddef15daa5fe3da6
Bikesh01 <bikeshkc999@gmail.com>
2024-09-04 20:57:58 +0545

    first  Navbar

---
+++ ias-client/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <div className="nav">
+    <a href="" type='active'>Home</a>
+    <a href="">About</a>
+    <a href="">Service</a>
+    <a href="">Course</a>
+    </div>
+    </>
+  )
+}
