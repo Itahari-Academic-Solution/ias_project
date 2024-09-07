8cefc435e03302d2d235929ded43785307a45862
Bikesh01 <bikeshkc999@gmail.com>
2024-09-07 13:45:06 +0545

    hero and contact

--- ias-client/src/Components/Home.jsx
+++ ias-client/src/Components/Home.jsx
@@ -5,20 +5,20 @@ export default function Home() {
     <>
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-stretch"> 
-          <div className="lg:flex-grow md:w-1/2 lg:pr-32 lg:p-10 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center bg-red-100 ">
-            <h1 className="title-font sm:text-4xl text-9xl mb-4 font-medium text-gray-900">
-              Before they sold out
-              <br className="hidden lg:inline-block" /> readymade gluten
+          <div className="lg:flex-grow md:w-1/2 lg:pr-32 lg:p-10 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center gap- ">
+            <h1 className="title-font sm:text-4xl lg:text-6xl mb-4 font-medium text-gray-900">
+            Unlock Your Potential 
+              <br className="hidden lg:inline-block" /> with Education
             </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. .
+            <p className="mb-8 leading-relaxed mt-4 mb-24">
+            Join us in shaping the leaders of tomorrow through innovative education and unwavering support.
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-[#BE1B2C] border-0 py-2 px-6 focus:outline-none hover:bg-[#A8162B] rounded text-lg">
-                Button
+              <button className="inline-flex text-white bg-[#BE1B2C] border-0 py-2 px-14 focus:outline-none hover:bg-[#A8162B] rounded-xl text-lg">
+                Courses
               </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
-                Button
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-14 focus:outline-none hover:bg-gray-200 rounded-xl text-lg">
+                Contact
               </button>
             </div>
           </div>
