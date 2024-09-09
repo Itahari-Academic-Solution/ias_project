0c9fc3b32f0949d76545c3ffe2f2041caff6dfe0
Bikesh01 <bikeshkc999@gmail.com>
2024-09-09 14:21:06 +0545

    in page courses course are added

--- ias-client/src/Components/Courses.jsx
+++ ias-client/src/Components/Courses.jsx
@@ -6,7 +6,153 @@ export default function Courses() {
     <div className="container mx-auto relative">
     <h2 className="text-gray-900 text-5xl mb-4 font-normal">Courses</h2>
 
-    
+    <div className="">
+    <div className="card grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-4 mt-10">
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://cdn.pixabay.com/photo/2019/02/06/16/32/architect-3979490_960_720.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+              Institute of Engineering 
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://cdn.pixabay.com/photo/2014/12/10/20/56/medical-563427_1280.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Medical Preparation Classes
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-FEATURE-powerful-apps-2120x1190:VP1-539x349" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Computer Office basic Training
+              </h2>
+
+              <p className="text-gray-700 ">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRd1FaHttul9B653rv0XbaNMBMJJHk8wJRcA&s" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                11/12 Crash course
+              </h2>
+
+              <p className="text-gray-700">
+                NEB (11/12) Crash Course (Science and Management)
+              </p>
+            </div>
+            
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://media.licdn.com/dms/image/D4D12AQHWjlf6CXDezA/article-cover_image-shrink_720_1280/0/1698039213268?e=2147483647&v=beta&t=9d8VyULomdIk9zkz_U9N_TE4_pUdFq1dFCglRh8fv_o" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+               Web development
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://i.pinimg.com/originals/c3/ec/11/c3ec11d1ded37a3989479f9740659272.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                11/12 Tution class
+              </h2>
+
+              <p className="text-gray-700">
+                NEB (11/12) Tution class 
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://cdn.dnaindia.com/sites/default/files/styles/full/public/2014/04/01/223991-coaching.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Tution class for School Level
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://ignitioncareerinstitute.com/wp-content/uploads/2024/01/WhatsApp-Image-2024-01-21-at-11.23.13_ca2a124e.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Bridge Course
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://lh6.googleusercontent.com/proxy/3tXt2Zx4JKy1LeXyYp3NeJCxuyHxHlWbJUkWL14Ji-I69DjORqHQOE0EGXRin4vBX7ImWJGIRNP8QeE4ZHJ2ZOEIXZNZ3yQgwbL_axJCsKqaEGCcbQ" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Bachelor and Masters Tution Classes
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://us.123rf.com/450wm/wirestock/wirestock2311/wirestock231116257/219193617-a-man-tapping-on-a-3d-rendering-of-flags-and-binary-code-international-data-networking-and.jpg?ver=6" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                Language Preparation
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://www.besonline.in/blog/wp-content/uploads/2020/09/PCL-Nursing.png" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                PCL Nursing
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://img.freepik.com/free-vector/pharmacology-technologies-background_1284-11968.jpg" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+               Pharmacy
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+            <div className=" flex flex-col  justify-center items-center p-8 rounded-xl boxshadow">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqizePiPSBNqj0NzG4_xfxBDhsf0M1OHrEhQ&s" className='rounded-xl' alt="" />
+              <h2 className="text-xl font-bold text-gray-800 mt-6 mb-2">
+                CTEVT Entrance Preparation
+              </h2>
+
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet consectetur.lorem8 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit.{" "}
+              </p>
+            </div>
+
+          </div>
+    </div>
     </div>
     </>
     
